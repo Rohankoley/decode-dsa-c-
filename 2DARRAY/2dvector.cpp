22fc2a71575c2455771cb809c6d18debbb9fe9ac
Rohan Koley <rohankoley777@gmail.com>
2026-02-16 16:51:04 +0530

    Updated dsa code

--- 2DARRAY/2dvector.cpp
+++ 2DARRAY/2dvector.cpp
@@ -1,1 +1,12 @@
- 
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector< vector<int> > v(3,vector<int> (4,3));
+    for(int i=0;i<v.size();i++){
+        for(int j=0;j<v[0].size();j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
