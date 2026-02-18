0901d48e1235157f1c1150d3891d4847eeb17252
Rohan Koley <rohankoley777@gmail.com>
2026-02-18 19:31:28 +0530

    Updated dsa code

---
+++ string/diffneigh.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"enter the string"<<endl;
+    getline(cin,str);
+    int count=0;
+    for(int i=0;i<str.length();i++){
+        if(str.length()==1) break;
+        if(i==0){
+            if(str[i]!=str[i+1]) count++;
+        }
+        else if(i==str.length()-1){
+            
+            if(str[i]!=str[i-1]) count++; 
+        }
+        else if(str[i-1]!=str[i]&&str[i]!=str[i+1]){
+            count++;
+        }
+        
+    }
+    cout<<count;
+}
---
+++ string/intro.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // char str[]={'a','b','c','d','e'};
+    // char str[]="abcde";
+    // for(int i=0;i<5;i++){
+    //     cout<<str[i]<<" ";
+    // }
+    string str ="rohan koley is a student";
+    cout<<str<<endl;    
+    cout<<str[0]<<endl;
+    string s;
+    // cin>>s;
+    // cout<<s;
+    getline(cin,s);
+    cout<<s;
+}
